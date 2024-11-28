7ce4f4f023bf369400de2b9665632ac8e9c6e66e
alexandre-bonnet <alexandre.bonnet@edu.esiee.fr>
2024-11-28 10:40:26 +0000

    ex7 part1

--- main.py
+++ main.py
@@ -15,8 +15,13 @@ def artcode_i(s):
     """
     
     # votre code ici
-
-    return [ ]
+    l = []
+    i = 1
+    while(s[i]==s[0]):
+        i+=1
+    ev = (s[0],i)
+    l.append(ev)
+    return l.append(artcode_i(s[i:]))
 
 
 def artcode_r(s):
@@ -30,7 +35,13 @@ def artcode_r(s):
     """
     
     # votre code ici
-
+    l = []
+    i = 0
+    while (s[i]==s[0]):
+        i+=1
+    ev = (s[0],i)
+    l.append(ev)
+    return l.append(artcode_i(s[i:]))
     # cas de base
     # recherche nombre de caractères identiques au premier
     # appel récursif
